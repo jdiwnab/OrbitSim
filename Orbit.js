c4e025dec84a8056dea764efad7b4756a6a3b667
adtraub <fairplaytechnologies@gmail.com>
2015-03-27 00:57:55 -0400

    Implement Saving and Loading Files
    Creates basic saving and loading functionality for files.

--- Orbit.js
+++ Orbit.js
@@ -9,6 +9,7 @@ function addEvent(o,e,f) {
     }
 }
 
+
 var orbit_data = new OrbitData();
 //orbit_data.genPlanets();
 
@@ -34,63 +35,71 @@ engine.removePlanetData = function(){
 }
 
 //Single save function that handles saving setup and saving state
-engine.saveData = function(){
+engine.saveData = function(storage){
     engine.removePlanetData();
     var planetList = [];
     for(i in orbit_data.planet_array){ 
-        localStorage["planet "+i+" name"] = orbit_data.planet_array[i].name;
-        localStorage["planet "+i+" radius"] = orbit_data.planet_array[i].radius;
-        localStorage["planet "+i+" mass"] = orbit_data.planet_array[i].mass;
-        localStorage["planet "+i+" color"] = orbit_data.planet_array[i].color;
-        localStorage["planet "+i+" posx"] = orbit_data.planet_array[i].pos.x;
-        localStorage["planet "+i+" posy"] = orbit_data.planet_array[i].pos.y;
-        localStorage["planet "+i+" posz"] = orbit_data.planet_array[i].pos.z;
-        localStorage["planet "+i+" velx"] = orbit_data.planet_array[i].vel.x;
-        localStorage["planet "+i+" vely"] = orbit_data.planet_array[i].vel.y;
-        localStorage["planet "+i+" velz"] = orbit_data.planet_array[i].vel.z;
-        localStorage["planet "+i+" startpos"] = orbit_data.planet_array[i].startpos.x;
-        localStorage["planet "+i+" startvel"] = orbit_data.planet_array[i].startvel.z;
+        storage["planet "+i+" name"] = orbit_data.planet_array[i].name;
+        storage["planet "+i+" radius"] = orbit_data.planet_array[i].radius;
+        storage["planet "+i+" mass"] = orbit_data.planet_array[i].mass;
+        storage["planet "+i+" color"] = orbit_data.planet_array[i].color;
+        storage["planet "+i+" posx"] = orbit_data.planet_array[i].pos.x;
+        storage["planet "+i+" posy"] = orbit_data.planet_array[i].pos.y;
+        storage["planet "+i+" posz"] = orbit_data.planet_array[i].pos.z;
+        storage["planet "+i+" velx"] = orbit_data.planet_array[i].vel.x;
+        storage["planet "+i+" vely"] = orbit_data.planet_array[i].vel.y;
+        storage["planet "+i+" velz"] = orbit_data.planet_array[i].vel.z;
+        storage["planet "+i+" startpos"] = orbit_data.planet_array[i].startpos.x;
+        storage["planet "+i+" startvel"] = orbit_data.planet_array[i].startvel.z;
         planetList.push("planet "+i);
     }
-    localStorage["planetList"] = JSON.stringify(planetList);
+    storage["planetList"] = JSON.stringify(planetList);
+    return storage;
 }
 
 
 //Allows for loading of planet data.
+//loadingState should be true if loading state and false if loading setup
 engine.loadData = function(inputData, loadingState){ 
     engine.id("loadSetup").disabled = true;
     engine.id("loadState").disabled = true;
-    var planetList = JSON.parse(inputData["planetList"]);
-    for(p in planetList){
-        if(loadingState){
-            engine.updateFromLoad(
-                inputData[planetList[p]+" name"],
-                inputData[planetList[p]+" radius"],
-                inputData[planetList[p]+" posx"],
-                inputData[planetList[p]+" velz"],
-                inputData[planetList[p]+" mass"],
-                inputData[planetList[p]+" color"],
-                inputData[planetList[p]+" posy"],
-                inputData[planetList[p]+" posz"], 
-                inputData[planetList[p]+" velx"],
-                inputData[planetList[p]+" vely"]
-            );
-        } else{
-            engine.updateFromLoad(
-                inputData[planetList[p]+" name"],
-                inputData[planetList[p]+" radius"],
-                inputData[planetList[p]+" startpos"],
-                inputData[planetList[p]+" startvel"],
-                inputData[planetList[p]+" mass"],
-                inputData[planetList[p]+" color"]           
-            );
+    engine.id("import").disabled = true;
+    if(inputData["planetList"] != undefined){
+        var planetList = JSON.parse(inputData["planetList"]);
+        for(p in planetList){
+            if(loadingState){
+                engine.updateFromLoad(
+                    inputData[planetList[p]+" name"],
+                    inputData[planetList[p]+" radius"],
+                    inputData[planetList[p]+" posx"],
+                    inputData[planetList[p]+" velz"],
+                    inputData[planetList[p]+" mass"],
+                    inputData[planetList[p]+" color"],
+                    inputData[planetList[p]+" posy"],
+                    inputData[planetList[p]+" posz"], 
+                    inputData[planetList[p]+" velx"],
+                    inputData[planetList[p]+" vely"]
+                );
+            } else{
+                engine.updateFromLoad(
+                    inputData[planetList[p]+" name"],
+                    inputData[planetList[p]+" radius"],
+                    inputData[planetList[p]+" startpos"],
+                    inputData[planetList[p]+" startvel"],
+                    inputData[planetList[p]+" mass"],
+                    inputData[planetList[p]+" color"]
+                );
+            }
         }
+    } else {
+        console.log("You have no saved data!")
     }
 }
 
 
-//basically a copy of the engine.addBody function with the unnecessary bits removed and allowing extra params
+//basically a copy of the engine.addBody function with the unnecessary bits removed and allowing extra arguments
 engine.updateFromLoad = function(name, radius, posx, velz, mass, color, posy, posz, velx, vely) {
+    //The following lines are a way of implementing default arguments
     if (typeof(posy)==='undefined') posy = 0;
     if (typeof(posz)==='undefined') posz = 0;
     if (typeof(velx)==='undefined') velx = 0;
@@ -100,6 +109,50 @@ engine.updateFromLoad = function(name, radius, posx, velz, mass, color, posy, po
     orbit_data.planet_array.push(body);
     orbit_data.addToTable(body);
     engine.reset();
+}
+
+//exports the data to a readable file
+
+engine.exportData = function(){
+    var textToWrite = JSON.stringify(engine.saveData({}));
+
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/JSON'});
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = engine.id("filename").value + ".json";
+    downloadLink.innerHTML = "Download File";
+    if (window.URL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+    downloadLink.click();
+}
+
+//imports data from a file
+engine.importData = function(){
+
+    var file = engine.id("import").files[0];
+    var reader = new FileReader();
+
+    //necessary due to asynchronous race condition stuff
+    reader.onload = function(e) {
+        var text = JSON.parse(reader.result);
+        engine.loadData(text, engine.id("stateOption").checked);
+    }
+
+    reader.readAsText(file);
+
 }
 
 engine.reset = function() {
@@ -313,8 +366,16 @@ engine.setupControlEvents = function() {
         engine.reset();
         return false;
     }, false);
-    engine.id("save").addEventListener('click', function(e) {
-        engine.saveData();
+    engine.id("saveLocal").addEventListener('click', function(e) {
+        engine.saveData(localStorage);
+        return false;
+    }, false);
+    engine.id("export").addEventListener('click', function(e) {
+        engine.exportData();
+        return false;
+    }, false);
+    engine.id("import").addEventListener('change', function(e) {
+        engine.importData();
         return false;
     }, false);
     engine.id("loadSetup").addEventListener('click', function(e) {
