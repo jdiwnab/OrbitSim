e5b2fd93764d211a338692e9d83c8cecbff5e283
jdiwnab <jdiwnab@users.noreply.github.com>
2015-05-17 21:12:20 -0400

    #15 code cleanup

--- rungekutta.js
+++ rungekutta.js
@@ -1,13 +1,3 @@
-/*engine.rkEval = function(pa, dt, d, array) {
-    var initPos = new Cart3(pa.pos);
-    var initVel = new Cart3(pa.vel);
-    var newPos = new Cart3(initPos.addTo(d.dx.mult(dt)));
-    var newVel = new Cart3(initVel.addTo(d.dv.mult(dt)));
-    var newAccl= engine.calcAccel(pa, newPos, array);
-    var output = new Derivitive(newVel, newAccl);
-    return output;
-}*/
-
 engine.rkIntegrate= function(pa, dt, array) {
 
     
@@ -15,22 +5,16 @@ engine.rkIntegrate= function(pa, dt, array) {
     var v1 = new Cart3(pa.vel);
     var a1 = engine.calcAccel(pa, x1, array);
     
-    var x2 = new Cart3().addTo(v1).multBy(0.5*dt).addTo(pa.pos);
-    //var x2 = new Cart3(v1.mult(0.5*dt).add(pa.pos));
-    var v2 = new Cart3().addTo(a1).multBy(0.5*dt).addTo(pa.vel);
-    //var v2 = new Cart3(a1.mult(0.5*dt).add(pa.vel));
+    var x2 = new Cart3(v1).multBy(0.5*dt).addTo(pa.pos);
+    var v2 = new Cart3(a1).multBy(0.5*dt).addTo(pa.vel);
     var a2 = engine.calcAccel(pa, x2, array);
 
-    var x3 = new Cart3().addTo(v2).multBy(0.5*dt).addTo(pa.pos);
-    //var x3 = new Cart3(v2.mult(0.5*dt).add(pa.pos));
-    var v3 = new Cart3().addTo(a2).multBy(0.5*dt).addTo(pa.vel);
-    //var v3 = new Cart3(a2.mult(0.5*dt).add(pa.vel));
+    var x3 = new Cart3(v2).multBy(0.5*dt).addTo(pa.pos);
+    var v3 = new Cart3(a2).multBy(0.5*dt).addTo(pa.vel);
     var a3 = engine.calcAccel(pa, x3, array);
     
-    var x4 = new Cart3().addTo(v3).multBy(dt).addTo(pa.pos);
-    //var x4 = new Cart3(v3.mult(dt).add(pa.pos));
-    var v4 = new Cart3().addTo(a3).multBy(dt).addTo(pa.vel);
-    //var v4 = new Cart3(a3.mult(dt).add(pa.vel));
+    var x4 = new Cart3(v3).multBy(dt).addTo(pa.pos);
+    var v4 = new Cart3(a3).multBy(dt).addTo(pa.vel);
     var a4 = engine.calcAccel(pa, x4, array);
     
     var xf = new Cart3().addTo(v2).addTo(v3).multBy(2).addTo(v1).addTo(v4).multBy(dt/6);
