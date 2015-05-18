0036628dfd283b076294e59cbfa53ebf00a8f583
jdiwnab <jdiwnab@users.noreply.github.com>
2015-05-17 21:15:18 -0400

    FPS calc change

--- render.js
+++ render.js
@@ -12,7 +12,7 @@ engine.perform = function(refresh) {
         if(engine.fps == 0) {
             engine.fps = fps;
         } else {
-            engine.fps = engine.fps + (fps - engine.fps)/engine.frame_count;
+            engine.fps = engine.fps + (fps - engine.fps)/100;
         }
     }
     if(!refresh) {
