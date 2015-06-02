8b6352834356149268d0d6e70e4754ef9d9ed7a9
jdiwnab <jdiwnab@users.noreply.github.com>
2015-06-02 15:37:44 -0400

    Issue #22 different gif encoder to speed things up

--- recording.js
+++ recording.js
@@ -2,11 +2,12 @@ engine.startRecord = function() {
     engine.id('record').textContent = 'Stop Rec';
     engine.recording = true;
     engine.recFrame = -1;
+    engine.encoder = new GIF({wokers:2, quality: 10, width: engine.xsize, height: engine.ysize});
     //engine.encoder = new GIFEncoder();
     //engine.encoder.setRepeat(0);
     //engine.encoder.setDelay(26);
     //engine.encoder.start();
-  	engine.gifworker = new Worker('lib/jsgif/animWorker.js');
+  	/*engine.gifworker = new Worker('lib/jsgif/animWorker.js');
     engine.recordFrame('start');
     
     engine.gifworker.onmessage = function(e) {
@@ -16,22 +17,26 @@ engine.startRecord = function() {
         if(gif_done) {
             engine.saveGif(e.data['frame_data']);
         }
-    };
+    };*/
+    engine.encoder.on('finished', function(blob) {
+        engine.saveGif(blob);
+    });
 
 }
 
 engine.recordFrame = function(status) {
     if(engine.recording) {
         //engine.encoder.addFrame(engine.ctx);
-        engine.recFrame ++;
+        /*engine.recFrame ++;
         var framelength;
         if(status == 'stop') {
             framelength = engine.recFrame;
         } else {
             framelength = engine.recFrame +1;
-        }
-        engine.gifworker.postMessage({"frame_index": engine.recFrame, "delay": 26, "frame_length":framelength, "height":engine.ysize, "width":engine.xsize, "imageData":engine.ctx.getImageData(0,0,engine.xsize, engine.ysize).data});
+        }*/
+        //engine.gifworker.postMessage({"frame_index": engine.recFrame, "delay": 26, "frame_length":framelength, "height":engine.ysize, "width":engine.xsize, "imageData":engine.ctx.getImageData(0,0,engine.xsize, engine.ysize).data});
         //engine.encoder.addFrame(engine.ctx);
+        engine.encoder.addFrame(engine.ctx, {copy: true, delay: 26});
     }
 }
 
@@ -39,11 +44,12 @@ engine.stopRecord = function() {
     engine.id('record').textContent = 'Record';
     //engine.encoder.finish();
     //engine.saveGif(engine.encoder.stream().getData());
-    engine.recordFrame('stop');
+    //engine.recordFrame('stop');
     engine.recording = false;
+    engine.encoder.render();
 }
 engine.saveGif = function(bin_data) {
-    var data = 'data:image/gif;base64,'+encode64(bin_data);
+    //var data = 'data:image/gif;base64,'+encode64(bin_data);
     var downloadLink = document.createElement("a");
     downloadLink.download = "OrbitAnimation.gif";
     downloadLink.innerHTML = "Download File";
@@ -51,13 +57,15 @@ engine.saveGif = function(bin_data) {
     {
         // Chrome allows the link to be clicked
         // without actually adding it to the DOM.
-        downloadLink.href = data;
+        //downloadLink.href = data;
+        downloadLink.href = window.URL.createObjectURL(blob);
     }
     else
     {
         // Firefox requires the link to be added to the DOM
         // before it can be clicked.
-        downloadLink.href = data;
+        //downloadLink.href = data;
+        downloadLink.href = window.URL.createObjectURL(blob);
         downloadLink.onclick = destroyClickedElement;
         downloadLink.style.display = "none";
         document.body.appendChild(downloadLink);
