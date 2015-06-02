f9091ff2ea3ec5acfecc3f26833b403a2459a5a3
jdiwnab <jdiwnab@users.noreply.github.com>
2015-06-02 12:37:17 -0400

    Issue #22 trying to fix encoding issue

--- recording.js
+++ recording.js
@@ -1,12 +1,23 @@
 engine.startRecord = function() {
     engine.id('record').textContent = 'Stop Rec';
     engine.recording = true;
+    engine.recFrame = 0;
     //engine.encoder = new GIFEncoder();
     //engine.encoder.setRepeat(0);
     //engine.encoder.setDelay(26);
     //engine.encoder.start();
   	engine.gifworker = new Worker('lib/jsgif/animWorker.js');
     engine.recordFrame('start');
+    
+    engine.gifworker.onmessage = function(e) {
+        var frame_index = e.data['frame_index'];
+        if(frame_index == 10) {
+            engine.saveGif(e.data['frame_data']);
+        } else {
+            engine.log('recording frame '+frame_index+' of '+engine.recFrame);
+            console.log('recording frame '+frame_index+' of '+engine.recFrame);
+        }
+    };
 
 }
 
@@ -14,46 +25,40 @@ engine.recordFrame = function(status) {
     if(engine.recording) {
         //engine.encoder.addFrame(engine.ctx);
         var frameindex, framelength;
-        if(status == 'start') {
-            frameindex = 0;
-            framelength = 10;
-        } else if(status == 'stop') {
-            frameindex = 10;
-            framelength = 10;
+        if(status == 'stop') {
+            framelength = engine.recFrame;
         } else {
-            frameindex = 1;
-            framelength = 10;
+            framelength = engine.recFrame +1;
         }
-        engine.gifworker.postMessage({"frame_index": frameindex, "delay": 26, "frame_length":framelength, "height":engine.ysize, "width":engine.xsize, "imageData":engine.ctx.getImageData(0,0,engine.xsize,engine.ysize).data});
+        engine.gifworker.postMessage({"frame_index": engine.recFrame, "delay": 26, "frame_length":framelength, "height":engine.ysize, "width":engine.xsize, "imageData":engine.ctx.getImageData(0,0,engine.xsize,engine.ysize).data});
     }
 }
 
 engine.stopRecord = function() {
     engine.id('record').textContent = 'Record';
     //engine.encoder.finish();
-    engine.gifworker.onmessage = function(e) {
-        var bin_data = e.data['frame_data'];
-        var data = 'data:image/gif;base64,'+encode64(bin_data);
-        var downloadLink = document.createElement("a");
-        downloadLink.download = "OrbitAnimation.gif";
-        downloadLink.innerHTML = "Download File";
-        if (window.URL != null)
-        {
-            // Chrome allows the link to be clicked
-            // without actually adding it to the DOM.
-            downloadLink.href = data;
-        }
-        else
-        {
-            // Firefox requires the link to be added to the DOM
-            // before it can be clicked.
-            downloadLink.href = data;
-            downloadLink.onclick = destroyClickedElement;
-            downloadLink.style.display = "none";
-            document.body.appendChild(downloadLink);
-        }
-    downloadLink.click();
-    }
     engine.recordFrame('stop');
     engine.recording = false;
+}
+engine.saveGif(bin_data) {
+    var data = 'data:image/gif;base64,'+encode64(bin_data);
+    var downloadLink = document.createElement("a");
+    downloadLink.download = "OrbitAnimation.gif";
+    downloadLink.innerHTML = "Download File";
+    if (window.URL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = data;
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = data;
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+    downloadLink.click();
 }
