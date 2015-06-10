66c077ae4e79991e5ef7b9328b8d16d386302a67
jdiwnab <jdiwnab@users.noreply.github.com>
2015-06-10 11:50:38 -0400

    Split input.js. Added delte option

---
+++ controls.js
@@ -0,0 +1,188 @@
+engine.setupControlEvents = function() {
+    /* Form Events */
+    engine.setupFancyControls();
+    $("#timestep").on({
+        slide: function(e) {
+            return engine.updateTimestep(e, this);
+        },
+        set: function(e) {
+            return engine.updateTimestep(e, this);
+        }   
+    });
+    $("#framestep").on({
+        slide: function(e) {
+            return engine.updateFramerate(e, this);
+        },
+        set: function(e) {
+            return engine.updateFramerate(e, this);
+        } 
+    });
+    engine.id("showHistory").addEventListener('change', function(e) {
+        if( engine.id("showHistory").checked ) {
+            engine.history = true;
+            engine.resetScaledHistory();
+        } else {
+            engine.history = false;
+        }
+    }, false);
+    engine.id("start").addEventListener('click', function(e) {
+        $('#start').toggleClass('stop');
+        engine.pause(e);
+        return false;
+    }, false);
+    engine.id("reset").addEventListener('click', function(e) {
+        engine.reset();
+        return false;
+    }, false);
+    engine.id("clear").addEventListener('click', function(e) {
+        engine.orbit_data.planet_array = [];
+        engine.reset();
+        return false;
+    }, false);
+    engine.id("record").addEventListener('click', function(e) {
+        $('#record').toggleClass('stop');
+        if(engine.recording) {
+            engine.stopRecord();
+        } else {
+            engine.startRecord();
+        }
+        return false;
+    }, false);
+    engine.id("saveLocal").addEventListener('click', function(e) {
+        engine.removePlanetData();
+        localStorage.planetList = engine.getSaveData();
+        return false;
+    }, false);
+    engine.id("export").addEventListener('click', function(e) {
+        engine.exportData();
+        return false;
+    }, false);
+    engine.id("csvexport").addEventListener('click', function(e) {
+        engine.csvexport();
+        return false;
+    }, false);
+    engine.id("loadImport").addEventListener('click', function(e) {
+        engine.importData();
+        return false;
+    }, false);
+    engine.id("loadSetup").addEventListener('click', function(e) {
+        engine.loadData(localStorage.planetList, false);
+        return false;
+    }, false);
+    engine.id("loadState").addEventListener('click', function(e) {
+        engine.loadData(localStorage.planetList, true);
+        return false;
+    }, false);
+    engine.id("loadPreset").addEventListener('click', function(e) {
+        engine.loadPreset(e);
+        return false;
+    }, false);
+    engine.id("loadRandom").addEventListener('click', function(e) {
+        engine.loadRandomPlanets(500);
+        return false;
+    }, false);
+    engine.id("loadLagrange").addEventListener('click', function(e) {
+        engine.loadLagrange();
+        return false;
+    }, false);
+    /*engine.id("precalculate").addEventListener('click', function(e) {
+        engine.precalculateDialog();
+        return false;
+    }, false);*/
+    /*engine.id("algo1").addEventListener('change', function(e) {
+        engine.algorithm = e.target.value;
+        return false;
+    }, false);
+    engine.id("algo2").addEventListener('change', function(e) {
+        engine.algorithm =  e.target.value;
+        return false;
+    }, false);
+    engine.id("algo3").addEventListener('change', function(e) {
+        engine.algorithm =  e.target.value;
+        return false;
+    }, false);*/
+    
+    //engine.orbit_data.createDataSets();
+    
+    //Drawer controls
+    engine.id("main-menu").addEventListener('click', function(e) {
+        $('#main-controls').toggleClass('show');
+        $('#main-menu').toggleClass('show');
+    }, false);
+    engine.id("preset-menu").addEventListener('click', function(e) {
+        $('#preset-controls').toggleClass('show');
+        $('#preset-menu').toggleClass('show');
+    }, false);
+    engine.id("lstorage-menu").addEventListener('click', function(e) {
+        $('#lstorage-controls').toggleClass('show');
+        $('#lstorage-menu').toggleClass('show');
+    }, false);
+    engine.id("fstorage-menu").addEventListener('click', function(e) {
+        $('#fstorage-controls').toggleClass('show');
+        $('#fstorage-menu').toggleClass('show');
+    }, false);
+    
+}
+
+engine.updateTimestep = function(e, control) {
+    engine.timeStep = 360 * control.vGet();
+    engine.id("stepvalue").textContent = control.vGet();
+    return false;
+}
+engine.updateFramerate = function(e, control) {
+    engine.stepsPerFrame = control.vGet();
+    engine.id("stepcount").textContent = control.vGet();
+    return false;
+}
+
+engine.setupFancyControls = function() {
+    $('#timestep').noUiSlider({
+        start: 10,
+        range: {
+            'min': [ 1 ],
+            'max': [100]
+        },
+        format: {
+            to: function(i) {
+                return Math.round(i);
+            },
+            from: function(i) {
+                return Math.round(i);
+            }
+        },
+        step: 1
+    });
+    $('#framestep').noUiSlider({
+        start: 1,
+        range: {
+            'min': [ 1 ],
+            'max': [500]
+        },
+        format: {
+            to: function(i) {
+                return Math.round(i);
+            },
+            from: function(i) {
+                return Math.round(i);
+            }
+        },
+        step: 1
+    });
+    engine.createForm();
+    engine.createPrecalculateForm();
+}
+
+engine.loadPreset = function(e) {
+    engine.loadObject(engine.orbit_data.presets[parseInt(engine.id('presetSelect').value)].planetArray, false);
+}
+
+engine.fireEvent = function(element, event) {
+    if ("createEvent" in document) {
+        var evt = document.createEvent("HTMLEvents");
+        evt.initEvent(event, false, true);
+        element.dispatchEvent(evt);
+    }
+    else {
+        element.fireEvent(event);
+    }
+}
---
+++ modals.js
@@ -0,0 +1,192 @@
+engine.newPlanetDialog = function() {
+    engine.id('is_edit').value = false;
+    engine.id('modal_head').textContent="New Object";
+    if(engine.orbit_data.planet_array[0] === undefined) {
+        new_pos = new Cart3(0,0,0);
+        new_vel = new Cart3(0,0,0);
+    } else {
+        //assume other ones orbit a centered sun at index 0, and assume a circular orbit
+        new_pos = engine.unscaleCoordinate(engine.mouseX, engine.mouseY);
+        // v = sqrt((G*M_sun)/R)  
+        new_vel = new Cart3(0,0,Math.sqrt(engine.orbit_data.planet_array[0].mass/new_pos.abs()));
+    }
+    $('#new_pos').val(new_pos.abs());
+    engine.id('pos_angle').value = Math.atan2(new_pos.y, new_pos.x) * 180 / Math.PI;
+    var velocity = new_vel.abs();
+    $('#new_vel').val(velocity);
+    engine.id('vel_angle').value = Math.atan2(new_pos.x, -1 * new_pos.y) * 180 / Math.PI;
+    engine.fireEvent(engine.id('pos_angle'), 'change');
+    engine.fireEvent(engine.id('vel_angle'), 'change');
+    engine.id('new_rad').value=6000;
+    engine.id('new_name').value='';
+    engine.id('new_fixed').checked = false;
+    /*setTimeout(function() {
+    //    document.getElementById('new_name').focus();
+    }, 300);*/
+    
+    var modal = $.remodal.lookup[$('[data-remodal-id=editmodal]').data('remodal')];
+    modal.open();
+}
+
+engine.editPlanetDialog = function(i) {
+    engine.id('is_edit').value = true;
+    engine.id('edit_index').value = i;
+    engine.id('modal_head').textContent="Edit Object";
+    
+    var p = engine.orbit_data.planet_array[i];
+    engine.id('new_name').value=p.name;
+    var pos = p.startpos.abs();
+    var pos_angle = Math.atan2(p.startpos.z, p.startpos.x) * 180 / Math.PI;
+    $('#new_pos').val(pos);
+    engine.id('pos_angle').value = pos_angle;
+    engine.fireEvent(engine.id('pos_angle'), 'change');
+    
+    $('#new_mass').val(p.mass);
+    
+    var vel = p.startvel.abs();
+    var vel_angle = Math.atan2(p.startvel.z, p.startvel.x) * 180 / Math.PI;
+    $('#new_vel').val(vel);
+    engine.id('vel_angle').value = vel_angle;
+    engine.fireEvent(engine.id('vel_angle'), 'change');
+    
+    engine.id('new_rad').value = p.radius;
+    engine.id('new_color').value=p.color;
+    engine.id('new_fixed').checked = p.fixed;
+    /*setTimeout(function() {
+        document.getElementById('new_name').focus();
+    }, 300);*/
+
+    var modal = $.remodal.lookup[$('[data-remodal-id=editmodal]').data('remodal')];
+    modal.open();
+}
+
+engine.createForm = function() {
+    $('#new_mass').noUiSlider({
+        start: 1.3275E+011,
+        range: {
+            'min': [ 1 ],
+            '25%': [1e+3],
+            '50%': [1e+6],
+            '75%': [1e+9],
+            'max': [1.4E+011]
+        },
+    });
+    $('#new_mass').noUiSlider_pips({
+        mode: 'range',
+        density: 3,
+        format: {
+            to: function(value) {
+                return value.toExponential();
+            },
+            from: function(value) {
+                return value.toExponential();
+            }
+        }
+    });
+    $('#new_mass').Link('lower').to($('#hidden_mass'));
+    $('#new_vel').noUiSlider({
+        start: 0,
+        range: {
+            'min': [ -50 ],
+            '50%': [0],
+            'max': [50]
+        },
+    });
+    $('#new_vel').noUiSlider_pips({
+        mode: 'range',
+        density: 3
+    });
+    $('#new_vel').Link('lower').to($('#hidden_vel'));
+    $('#new_pos').noUiSlider({
+        start: 0,
+        range: {
+            'min': [ 0 ],
+            '33%': [100000000],
+            '66%': [1000000000],
+            'max': [10000000000]
+        },
+    });
+    $('#new_pos').noUiSlider_pips({
+        mode: 'range',
+        density: 3,
+        format: {
+            to: function(value) {
+                return value.toExponential();
+            },
+            from: function(value) {
+                return value.toExponential();
+            }
+        }
+    });
+    $('#new_pos').Link('lower').to($('#hidden_pos'));
+    engine.id("vel_angle").addEventListener('change', function(e) {
+        $('#vel_arrow').css('transform', 'rotate('+e.target.value+'deg)')
+    }, false);
+    engine.id("pos_angle").addEventListener('change', function(e) {
+        $('#pos_arrow').css('transform', 'rotate('+e.target.value+'deg)')
+    }, false);
+    var colors = document.getElementById('new_color');
+    for(var i = 0; i<orbit_data.planetColors.length; i++) {
+        var option = document.createElement('option');
+        option.text=orbit_data.planetColors[i];
+        option.value=orbit_data.planetColors[i];
+        colors.add(option);
+    }
+    
+    $(document).on('confirm', '#editmodal', function () {
+        if(engine.id('is_edit').value === "false") {
+            engine.submitNewForm();
+        } else {
+            engine.submitEditForm();
+        }
+    });
+    engine.id('delete_object').addEventListener('click', function() {
+        if(engine.id('is_edit').value === "true") {
+            engine.orbit_data.planet_array.splice(engine.id('edit_index').value, 1);
+            engine.reset();
+        }
+        $.remodal.lookup[$('[data-remodal-id=editmodal]').data('remodal')].close();
+    }, false);
+}
+
+engine.submitNewForm = function() {
+    var vel = parseFloat(engine.id('hidden_vel').value);
+    var vel_angle = parseFloat(engine.id('vel_angle').value) * Math.PI / 180;
+    var vel_x = vel * Math.cos(vel_angle);
+    var vel_y = vel * Math.sin(vel_angle);
+    var pos = parseFloat(engine.id('hidden_pos').value);
+    var pos_angle = parseFloat(engine.id('pos_angle').value) * Math.PI / 180;
+    var pos_x = pos * Math.cos(pos_angle);
+    var pos_y = pos * Math.sin(pos_angle);
+    engine.addPlanet(engine.id('new_name').value, new Cart3(pos_x, 0, pos_y), parseFloat(engine.id('hidden_mass').value), new Cart3(parseFloat(vel_x), 0, parseFloat(vel_y)), engine.id('new_color').value, parseFloat(engine.id('new_rad').value), engine.id('new_fixed').checked);
+}
+
+engine.submitEditForm = function() {
+    var p = engine.orbit_data.planet_array[engine.id('edit_index').value];
+    p.name = engine.id('new_name').value;
+    var pos = parseFloat(engine.id('hidden_pos').value);
+    var pos_angle = parseFloat(engine.id('pos_angle').value) * Math.PI / 180;
+    p.startpos.x = pos * Math.cos(pos_angle);
+    p.startpos.z = pos * Math.sin(pos_angle);
+    p.mass = parseFloat(engine.id('hidden_mass').value);
+    p.startmass = parseFloat(engine.id('hidden_mass').value);
+    var angle = parseFloat(engine.id('vel_angle').value) * Math.PI / 180;
+    var vel = parseFloat(engine.id('hidden_vel').value);
+    p.startvel.x = vel * Math.cos(angle);
+    p.startvel.z = vel * Math.sin(angle);
+    p.color = engine.id('new_color').value;
+    p.radius = parseFloat(engine.id('new_rad').value);
+    p.fixed = engine.id('new_fixed').checked;
+    engine.reset();
+}
+
+engine.precalculateDialog = function() {
+    var modal = $.remodal.lookup[$('[data-remodal-id=precalcmodal]').data('remodal')];
+    modal.open();
+}
+
+engine.createPrecalculateForm = function() {
+    $(document).on('confirm', '#precalcmodal', function () {
+        engine.precalculate(parseInt(engine.id('precalc_timestep').value), parseInt(engine.id('precalc_timespan').value));
+    });
+}
