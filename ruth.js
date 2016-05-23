b7237200cad8e3cbc0dce3390bdd62ea7c545b6f
jdiwnab <jdiwnab@users.noreply.github.com>
2016-05-23 14:51:57 -0400

    Create ruth.js

---
+++ ruth.js
@@ -0,0 +1,23 @@
+engine.ruth2Integrate = function(pa, dt, array) {
+  c = [0.0, 1.0];
+  d = [0.5, 0.5];
+  engine.ruthIntegrate(pa, dt, array, c, d);
+}
+
+engine.ruth3Integrate = function(pa, dt, array) {
+  c = [7.0/24.0,  3.0/4.0,  -1.0/24.0 ];
+  d = [2.0/3.0,   -2.0/3.0, 1.0       ];
+  engine.ruthIntegrate(pa, dt, array, c, d);
+}
+
+engine.ruthIntegrate(pa, dt, array, c, d) {
+  var pos = new Cart3(pa.pos);
+  var vel = new Cart3(pa.vel);
+  for(var i = 0; i< c.length(), i++) {
+    var accel = engine.calcAccel(pa, pos, array);
+    vel.addTo(accel.multBy(c[i] * dt));
+    pos.addTo(vel.multBy(d[i] * dt));
+  }
+  pa.deltaV = vel.subFrom(pa.vel);
+  pa.deltaX = pos.subFrom(pa.pos);
+}
